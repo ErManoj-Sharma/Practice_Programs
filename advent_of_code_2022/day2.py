8aa1205f14d7ccd4ce42ba9b6a268b8db7b4d503
ErManoj-Sharma <manoj.sharma@ongraphmail.com>
2023-06-29 20:18:42 +0530

    Day 4 part 2

--- advent_of_code_2022/day2.py
+++ advent_of_code_2022/day2.py
@@ -11,7 +11,7 @@ def read_file(filename):
           second_chars.append(line[2])
   return first_chars,second_chars
 
-def calculate_score(first,second):
+def calculate_score_part1(first,second):
   win_score = 0
   for i in range(2500):
     if first[i] == 'A':              # Rock
@@ -37,11 +37,36 @@ def calculate_score(first,second):
        else:                             #scssor
           win_score = win_score +  6   # 3+3
   return win_score
-        
+
+def calculate_score_part2(first,second):
+  win_score = 0
+  for i in range(2500):
+    if first[i] == 'A':              # Rock
+       if second[i] == 'X':          # rock
+          win_score = win_score + 3   #(2+3)
+       elif second[i] == 'Y':          #paper
+          win_score = win_score + 4   #(2+6)
+       else:
+          win_score = win_score + 8   #(scssor,(3+0))
+    
+    elif first[i] == 'B':                 # paper
+       if second[i] == 'X':            #rock
+          win_score = win_score + 1   #(1+0)
+       elif second[i] == 'Y':           #paper
+          win_score = win_score + 5    #(2+3)
+       else:                             #scssor
+          win_score = win_score +  9   # 3+6
+    elif first[i] == 'C':
+       if second[i] == 'X':            #rock
+          win_score = win_score + 2   #(1+6)
+       elif second[i] == 'Y':           #paper
+          win_score = win_score + 6    #(2+0)
+       else:                             #scssor
+          win_score = win_score + 7   # 3+3
+  return win_score
 
 first , second = read_file('day2.txt')
-# print(len(first))
-# print(len(second))
-result = calculate_score(first,second)
+result = calculate_score_part1(first,second)
 print(result)
-# Answer 9241
+result = calculate_score_part2(first,second)
+print(result)
--- advent_of_code_2022/day4.py
+++ advent_of_code_2022/day4.py
@@ -14,15 +14,23 @@ def make_two_array(lines):
     list2.append(end)
   return list1,list2
 
-def count(list1,list2):
+def part1_count(list1,list2):
   sum = 0
   for i in range(len(list1)):
     if(list1[i][0] <= list2[i][0] and list1[i][1] >= list2[i][1]) or (list1[i][0] >= list2[i][0] and list1[i][1] <= list2[i][1]):
-      print(list1[i][0], list1[i][1],"-",list2[i][0],list2[i][1])
-      print(sum)
       sum = sum + 1
   return sum
+
+def part2_count(list1,list2):
+  sum = 0
+  for i in range(len(list1)):
+    if(list1[i][0] < list2[i][0] and list1[i][1] < list2[i][0]) or (list1[i][0] > list2[i][0]  and list1[i][0] > list2[i][1]):
+      sum = sum + 1
+  return sum
+  
 lines = read_file('day4.txt')
 list1,list2 = make_two_array(lines)
-sum = count(list1,list2)
-print("sum is:",sum)
+part1_ans = part1_count(list1,list2)
+print("sum is:",part1_ans)
+part2ans = part2_count(list1,list2)
+print("sum2 is :",len(list1) - part2ans)
