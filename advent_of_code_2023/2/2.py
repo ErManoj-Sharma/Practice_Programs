f6c7efe0bf513a4d0af72f46f053be9edd084cbd
manoj <mmmanojsharma201611@gmail.com>
2023-12-20 13:26:53 +0530

    Day 2 2023

--- advent_of_code_2023/2/2.py
+++ advent_of_code_2023/2/2.py
@@ -4,31 +4,61 @@ def read_file():
     f.close()
     return lines
 
-def make_dict(l, index):
-    global my_dict
-    print("Index: ",index, " List: ", l)
-    # length = int(len(l))
-    # # print(length)
-    # for i in range(0,1):
-    #     arr = l[i].split(',')
-    #     arr_len = len(arr)
-    #     print(arr)
-    #     # print(arr_len)
-    #     for i in range(0,arr_len):
-    #         if arr[i].strip().split(' ')[1] == 'red':
-    #             arr[i].strip().split(' ')[0]
-    #         if arr[i].strip().split(' ')[1] == 'blue':
-    #             arr[i].strip().split(' ')[0]
-    #         if arr[i].strip().split(' ')[1] == 'green':
-    #             arr[i].strip().split(' ')[0]
+def check_valid_game(games):
+    status = True
+    game_len = len(games)
+    for i in range(game_len):
+        sub_game = games[i].strip().split(',')
+        sub_game_len = len(sub_game)
+        for j in range(sub_game_len):
+            if sub_game[j].strip().split()[1] == 'red' and int(sub_game[j].strip().split()[0]) > 12:
+                status=False
+                return status 
+            elif sub_game[j].strip().split()[1] == 'blue' and int(sub_game[j].strip().split()[0]) > 14:
+                status=False
+                return status 
+            elif sub_game[j].strip().split()[1] == 'green' and int(sub_game[j].strip().split()[0]) > 13:
+                status=False
+                return status 
+    return status
 
-green_ball = 13
-blue_ball = 14
-red_ball = 12
-my_dict = {} 
+def make_power_set(games):
+    max_red = 0
+    max_green=0
+    max_blue=0
+    game_len = len(games)
+    for i in range(game_len):
+        sub_game = games[i].strip().split(',')
+        sub_game_len = len(sub_game)
+        for j in range(sub_game_len):
+            if sub_game[j].strip().split()[1] == 'red' and int(sub_game[j].strip().split()[0]) > max_red:
+                max_red = int(sub_game[j].strip().split()[0])
+            elif sub_game[j].strip().split()[1] == 'blue' and int(sub_game[j].strip().split()[0]) > max_blue:
+                max_blue = int(sub_game[j].strip().split()[0])
+            elif sub_game[j].strip().split()[1] == 'green' and int(sub_game[j].strip().split()[0]) > max_green:
+                max_green = int(sub_game[j].strip().split()[0])
+    return max_green * max_blue * max_red
+        
+def possbile_game(l,index):
+    global final_array
+    global final_array2
+    status = check_valid_game(l)
+    if status:
+        final_array.append(index)
+    ans = make_power_set(l)
+    final_array2.append(ans)
+
 lines_array = read_file()
 i = 0
+final_array= []
+final_array2 = []
 for line in lines_array:
     i = i+1
-    # print(line[8:].split(";"))
-    make_dict(line[8:].split(";"), i)
+    possbile_game(line[8:].split(";"), i)
+# Part 1
+# print(final_array)
+print("Part 1: ",sum(final_array))
+
+# Part 2
+# print(final_array2)
+print("Part 2:",sum(final_array2))
